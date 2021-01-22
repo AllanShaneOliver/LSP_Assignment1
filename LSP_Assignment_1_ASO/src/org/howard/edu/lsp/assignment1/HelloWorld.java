b557b2264a78f1942921cf9d105ed0a7aec73b4f
Allan Shane Oliver <allanshane.oliver@bison.howard.edu>
2021-01-21 22:58:53 -0600

    "Hello World" Program written in Java by Allan Shane Oliver

---
+++ LSP_Assignment_1_ASO/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+//Written by Allan Shane Oliver
+
+public class HelloWorld { //Class declaration
+
+	public static void main(String[] args) { //main function is void: returns nothing
+		System.out.println("Hello World"); //Java printing and/or output method
+	}
+
+}
